5c0e51356f8f27588edb899a6a750d596cd69b8d
Evgeniy <charizardik64@gmail.com>
2025-05-20 16:26:54 +0300

    button and modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import Modal from "./components/Modal/Modal.tsx";
+import {Button} from "./components/Button/Button.tsx";
+import {AnimatePresence} from 'motion/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isModal, setIsModal] = useState(true)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={'block neumorphismAlt'}>
+        <Button
+          title={`${!isModal ? 'Open' : 'Close'} Modal`}
+          onClick={() => setIsModal(prevState => !prevState)
+          }
+        />
+
+        <AnimatePresence>
+          {isModal && <Modal/>}
+        </AnimatePresence>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,20 @@
+import {motion} from "motion/react";
+
+export default function Modal() {
+  return (
+    <motion.div
+      className={'modal neumorphism'}
+      initial={{scale: 0}}
+      animate={{scale: 1}}
+      exit={
+      { scale: 0,
+        opacity: 0 }}
+      transition={{duration: .5}}
+    >
+      <h1>Hello!</h1>
+      <h2>I'm Modal Component</h2>
+      <h3>Nice to meet you!</h3>
+    </motion.div>
+
+  )
+}
